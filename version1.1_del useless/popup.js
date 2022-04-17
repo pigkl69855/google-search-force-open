c8139e1a5528faa37e6ce5cca280c3ee3ea4afc4
TSAI HSIN JUI <50133946+pigkl69855@users.noreply.github.com>
2022-04-17 15:04:27 +0800

    Delete version1.1_del useless directory

--- version1.1_del useless/popup.js
+++
@@ -1,7 +0,0 @@
-var getSelectedTab = (tab) => {
-  var tabId = tab.id;
-  var sendMessage = (messageObj) => chrome.tabs.sendMessage(tabId, messageObj);
-  document.getElementById('openAll').addEventListener('click', () => sendMessage({ action: 'OPENALL' }))
-
-}
-chrome.tabs.getSelected(null, getSelectedTab);
