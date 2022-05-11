460cc04d790921eee7fdf0456db228d58a7e2fa2
TSAI HSIN JUI <50133946+pigkl69855@users.noreply.github.com>
2022-05-11 21:52:56 +0800

    Add files via upload

---
+++ version1.8_add duckDuckGo/popup.js
@@ -0,0 +1,24 @@
+var getSelectedTab = (tab) => {
+
+  var tab = tab[0];
+  var sendMessage = (messageObj) => chrome.tabs.sendMessage(tab.id, messageObj);
+  document.getElementById('openAll').addEventListener('click', async () => {
+
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+      let url = tabs[0].url;
+      if (url.includes("google.com")) {
+        sendMessage({ action: 'OPENALL_GOOGLE' })
+      }
+      if (url.includes("forum.gamer.com.tw")) {
+        sendMessage({ action: 'OPENALL_FORUM_GAMER' })
+      }
+      if (url.includes("duckduckgo.com")) {
+        sendMessage({ action: 'OPENALL_DUCK_DUCK_GO' })
+      }
+    });
+  }
+
+  )
+
+}
+chrome.tabs.query({active: true, currentWindow: true}, getSelectedTab);
